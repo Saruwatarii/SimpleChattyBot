48978176ee3112a17dc88bba5381bb966151dc27
Tim Aadland <potetstapp@gmail.com>
2020-07-16 14:07:46 +0200

    Added dialogue with greetings between the bot and the user.

--- chatty_bot.py
+++ chatty_bot.py
@@ -1,10 +1,23 @@
 '''
 This program is a simple chatty bot which help you study programming.
 First stage: a bot the display a greeting, its name and the date of its creation
+Second tage: Implement input from the user, a small dialouge with greetings
 '''
-
+'''
+First stage: 
 bot_name = "Alfred"
 birth_year = 2020
 
 print(f"Hello! My name is {bot_name}.")
 print(f"I was created in {birth_year}.")
+'''
+# Second tage
+bot_name = "Aid"
+bot_age = 2020
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {bot_age}.")
+
+print("Please, remind your name.")
+user_name = input()
+print(f"What a great name you have, {user_name}!")
