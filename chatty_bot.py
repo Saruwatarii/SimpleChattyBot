18466ca64a3a3961e06b2f71ea056aed0f563d8b
Tim Aadland <potetstapp@gmail.com>
2020-07-16 15:02:57 +0200

    Added a multiple-choice test, and recator each stage into function for easy readability, and can choose which function to run.

--- chatty_bot.py
+++ chatty_bot.py
@@ -4,6 +4,8 @@ First stage: a bot the display a greeting, its name and the date of its creation
 Second stage: Implement input from the user, a small dialogue with greetings
 Third stage: adding a guessing game that will predict the user age.
 Fourth stage: Learning the bot to count from 0 to any positive number the user enter.
+Fifth stage: Give the user a multiple-choice test about programming, will repeat until the user answer correctly.
+Lastly: refactoring each staged into function for easy readability.
 '''
 '''
 First stage: 
@@ -14,37 +16,65 @@ print(f"Hello! My name is {bot_name}.")
 print(f"I was created in {birth_year}.")
 '''
 # Second stage
-bot_name = "Aid"
-bot_age = 2020
+def greeting(bot_name, bot_age):
+    print(f"Hello! My name is {bot_name}.")
+    print(f"I was created in {bot_age}.")
 
-print(f"Hello! My name is {bot_name}.")
-print(f"I was created in {bot_age}.")
-print("Please, remind your name.")
 
-user_name = input()
+def remind_name():
+    print("Please, remind your name.")
+    user_name = input()
+    print(f"What a great name you have, {user_name}!")
 
-print(f"What a great name you have, {user_name}!")
 # Third stage
-print("Let me quess your age.")
-print("Enter remainders of dividing your age by 3, 5 and 7.")
+def guesging_age():
+    print("Let me quess your age.")
+    print("Enter remainders of dividing your age by 3, 5 and 7.")
 
-num1 = int(input())
-num2 = int(input())
-num3 = int(input())
+    num1 = int(input())
+    num2 = int(input())
+    num3 = int(input())
 
-remainder3 = num1 % 3
-remainder5 = num2 % 5
-remainder7 = num3 % 7
-# Only works if the user types in a number between 0 and 104.
-quessing_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    remainder3 = num1 % 3
+    remainder5 = num2 % 5
+    remainder7 = num3 % 7
+    # Only works if the user types in a number between 0 and 104.
+    quessing_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
 
-print(f"Your age is {quessing_age}; that's a good time to start programming")
+    print(f"Your age is {quessing_age}; that's a good time to start programming")
+
+def count():
 #Fourth stage
-print("Now I will prove to you that I can count to any number you want.")
+    print("Now I will prove to you that I can count to any number you want.")
 
-user_number = int(input())
+    user_number = int(input())
 
-for count in range(user_number + 1):
-    print(f"{count} !")
+    for count in range(user_number + 1):
+        print(f"{count} !")
 
-print("Completed, have a nice day!")
+def multiple_choice_test():
+    print("Let's test your programming knowledge.")
+    print("why do we use methods?")
+    print('''1. To repeat a statement multiple times.
+    2. To decompose a program into several small subroutines.
+    3. To determine the execution time of a program.
+    4. To interrupt the execution of a program.
+    ''')
+
+    while True:
+        multiple_choice = int(input())
+        if multiple_choice == 2:
+            print("Completed, have a nice day!")
+            break
+        else:
+            print("Please, try again")
+
+def end():
+    print("Congratulations, have a nice day!")
+
+greeting('Aid', '2020')  # change it as you need
+remind_name()
+guesging_age()
+count()
+multiple_choice_test()
+end()
