997f1bee7ed41585f5793cbef44ccb90261b7b18
Tim Aadland <potetstapp@gmail.com>
2020-07-16 13:56:04 +0200

    Added decription and the variables name and year and printed a greeting

---
+++ chatty_bot.py
@@ -0,0 +1,10 @@
+'''
+This program is a simple chatty bot which help you study programming.
+First stage: a bot the display a greeting, its name and the date of its creation
+'''
+
+bot_name = "Alfred"
+birth_year = 2020
+
+print(f"Hello! My name is {bot_name}.")
+print(f"I was created in {birth_year}.")
