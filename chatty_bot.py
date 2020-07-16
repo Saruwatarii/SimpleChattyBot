295d32f420e3aedb2e01ece4d36056a6ba8b4e1e
Tim Aadland <potetstapp@gmail.com>
2020-07-16 14:26:15 +0200

    Added THird stage which include a queesing game that uses a remainder trick to find your age. But it got to be within 0 and 104 for it to work.

--- chatty_bot.py
+++ chatty_bot.py
@@ -2,6 +2,7 @@
 This program is a simple chatty bot which help you study programming.
 First stage: a bot the display a greeting, its name and the date of its creation
 Second stage: Implement input from the user, a small dialogue with greetings
+Third stage: adding a guessing game that will predict the user age.
 '''
 '''
 First stage: 
@@ -21,3 +22,19 @@ print(f"I was created in {bot_age}.")
 print("Please, remind your name.")
 user_name = input()
 print(f"What a great name you have, {user_name}!")
+
+# Third stage
+print("Let me quess your age.")
+print("Enter remainders of dividing your age by 3, 5 and 7.")
+
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+remainder3 = num1 % 3
+remainder5 = num2 % 5
+remainder7 = num3 % 7
+# Only works if the user types in a number between 0 and 104.
+quessing_age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+
+print(f"Your age is {quessing_age}; that's a good time to start programming")
